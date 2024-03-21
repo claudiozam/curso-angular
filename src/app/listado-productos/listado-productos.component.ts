744056f850c9cf1b55623bfb4dac6bffc926cac8
claudio z <claudio.zamoszczyk@gmail.com>
2024-03-21 08:18:12 -0300

    ejemplo de pipes

--- src/app/listado-productos/listado-productos.component.ts
+++ src/app/listado-productos/listado-productos.component.ts
@@ -1,38 +1,50 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormProductoComponent } from '../form-producto/form-producto.component';
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { CapitalizePipe } from '../capitalize.pipe';
+
+
+export interface Producto {
+  id: number,
+  nombre: string,
+  precio: number,
+  fechaAlta: Date
+}
 
 @Component({
   selector: 'app-listado-productos',
   standalone: true,
-  imports: [FormProductoComponent],
+  imports: [FormProductoComponent, UpperCasePipe, CurrencyPipe, CapitalizePipe, DatePipe],
   templateUrl: './listado-productos.component.html',
   styleUrl: './listado-productos.component.css'
 })
 export class ListadoProductosComponent {
 
-  @Output() updateProducto = new EventEmitter<any>(); 
+  @Output() updateProducto = new EventEmitter<Producto>(); 
 
-  productoActual : any = null;
+  productoActual!: Producto;
 
-  productos = [{
+  productos : Array<Producto> = [{
     id: 1,
     nombre: 'Test 1',
-    precio: 1000
+    precio: 1000,
+    fechaAlta: new Date()
   }, {
     id: 2,
     nombre: 'Test 2',
-    precio: 2000
+    precio: 2000,
+    fechaAlta: new Date()
   }];
 
 
-  eventoClickVer(p : any) {
+  eventoClickVer(p : Producto) {
     console.log(p.id + ' ' + p.nombre);
     this.productoActual = p;
     this.updateProducto.emit(p);
   }
 
-  recibirProducto(producto : any) {
+  recibirProducto(producto : Producto) {
     this.productos.push(producto);
   }
 
